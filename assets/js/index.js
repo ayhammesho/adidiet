b6f90ac3f3467e3f8a6c5cc526a99dbe5e068795
ayhmmesho <ayhammesho@gmail.com>
2022-09-15 14:03:30 +0300

    update index

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,35 @@
+// قائمة التنقل عند الشاشات الصغيرة
+
+const offcanvasEnd = document.querySelector(".offcanvas-end");
+const navbarToggler = document.querySelector(".navbar-toggler");
+const btnClose = document.querySelector(".btn-close");
+const offcanvasBackdrop = document.querySelector(".offcanvas-backdrop");
+
+offcanvasBackdrop.addEventListener("click", function (e) {
+  offcanvasBackdrop.classList.add("fade");
+  offcanvasBackdrop.classList.remove("show");
+  offcanvasEnd.style.transform = "translateX(100%)";
+  document.body.style.overflow = "auto";
+  offcanvasBackdrop.style.display = "none";
+});
+
+navbarToggler.addEventListener("click", function (e) {
+  offcanvasEnd.classList.add("show");
+  offcanvasEnd.style.transform = "translateX(0)";
+  document.body.style.overflow = "hidden";
+  offcanvasBackdrop.style.display = "block";
+  offcanvasBackdrop.classList.add("show");
+  offcanvasBackdrop.classList.remove("fade");
+});
+
+btnClose.addEventListener("click", function (e) {
+  offcanvasEnd.style.transform = "translateX(100%)";
+  offcanvasBackdrop.classList.add("fade");
+  offcanvasBackdrop.classList.remove("show");
+  offcanvasBackdrop.style.display = "none";
+  document.body.style.overflow = "auto";
+});
+
 // قسم المجربون كود التنقل
 
 var swiper = new Swiper(".mySwiper", {
@@ -12,67 +44,116 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-// ألية الدفع paypal
-const fundingSources = [
-  paypal.FUNDING.PAYPAL,
-  paypal.FUNDING.PAYLATER,
-  paypal.FUNDING.CARD,
-];
+// ألية الدفع paypal4
 
-for (const fundingSource of fundingSources) {
-  const paypalButtonsComponent = paypal.Buttons({
-    fundingSource: fundingSource,
+function initPayPalButton() {
+  paypal
+    .Buttons({
+      style: {
+        shape: "pill",
+        color: "gold",
+        layout: "vertical",
+        label: "paypal",
+      },
 
-    // optional styling for buttons
-    // https://developer.paypal.com/docs/checkout/standard/customize/buttons-style-guide/
-    style: {
-      shape: "rect",
-      height: 40,
-    },
+      createOrder: function (data, actions) {
+        return actions.order.create({
+          purchase_units: [{ amount: { currency_code: "USD", value: 1 } }],
+        });
+      },
 
-    // set up the transaction
-    createOrder: (data, actions) => {
-      // pass in any options from the v2 orders create call:
-      // https://developer.paypal.com/api/orders/v2/#orders-create-request-body
-      const createOrderPayload = {
-        purchase_units: [
-          {
-            amount: {
-              value: "88.44",
-            },
-          },
-        ],
-      };
+      onApprove: function (data, actions) {
+        return actions.order.capture().then(function (orderData) {
+          // Full available details
+          console.log(
+            "Capture result",
+            orderData,
+            JSON.stringify(orderData, null, 2)
+          );
 
-      return actions.order.create(createOrderPayload);
-    },
+          // // Show a success message within this page, e.g.
+          const element = document.getElementById("paypal-button-container");
+          element.innerHTML = "";
+          element.innerHTML = "<h3>Thank you for your payment!</h3>";
+          // actions.redirect("/thank_you.html");
 
-    // finalize the transaction
-    onApprove: (data, actions) => {
-      const captureOrderHandler = (details) => {
-        const payerName = details.payer.name.given_name;
-        console.log("Transaction completed!");
-      };
+          // Or go to another URL:  actions.redirect('thank_you.html');
+        });
+      },
 
-      return actions.order.capture().then(captureOrderHandler);
-    },
-
-    // handle unrecoverable errors
-    onError: (err) => {
-      console.error(
-        "An error prevented the buyer from checking out with PayPal"
-      );
-    },
-  });
-
-  if (paypalButtonsComponent.isEligible()) {
-    paypalButtonsComponent.render("#paypal-button-container").catch((err) => {
-      console.error("PayPal Buttons failed to render");
-    });
-  } else {
-    console.log("The funding source is ineligible");
-  }
+      onError: function (err) {
+        alert("حدث خطأ في الدفع أعد المحاولة");
+        console.log(err);
+      },
+    })
+    .render("#paypal-button-container");
 }
+initPayPalButton();
+
+// // old code
+// const fundingSources = [
+//   paypal.FUNDING.PAYPAL,
+//   paypal.FUNDING.PAYLATER,
+//   paypal.FUNDING.CARD,
+// ];
+
+// for (const fundingSource of fundingSources) {
+//   const paypalButtonsComponent = paypal.Buttons({
+//     fundingSource: fundingSource,
+
+//     // optional styling for buttons
+//     // https://developer.paypal.com/docs/checkout/standard/customize/buttons-style-guide/
+//     style: {
+//       shape: "rect",
+//       height: 40,
+//     },
+
+//     // set up the transaction
+//     createOrder: (data, actions) => {
+//       // pass in any options from the v2 orders create call:
+//       // https://developer.paypal.com/api/orders/v2/#orders-create-request-body
+//       const createOrderPayload = {
+//         purchase_units: [
+//           {
+//             amount: {
+//               // هي لتغير سعر الدايت (السعر بالدولار)
+//               value: "37.99",
+//             },
+//           },
+//         ],
+//       };
+
+//       return actions.order.create(createOrderPayload);
+//     },
+
+//     // finalize the transaction
+//     onApprove: (data, actions) => {
+//       const captureOrderHandler = (details) => {
+//         const payerName = details.payer.name.given_name;
+//         console.log("Transaction completed!");
+//         actions.redirect("thank_you.html");
+//       };
+
+//       return actions.order.capture().then(captureOrderHandler);
+//     },
+
+//     // handle unrecoverable errors
+//     onError: (err) => {
+//       console.error(
+//         "An error prevented the buyer from checking out with PayPal"
+//       );
+//       alert("حدث خطأ في الدفع أعد المحاولة");
+//     },
+//   });
+
+//   if (paypalButtonsComponent.isEligible()) {
+//     paypalButtonsComponent.render("#paypal-button-container").catch((err) => {
+//       console.error("PayPal Buttons failed to render");
+//     });
+//   } else {
+//     console.log("The funding source is ineligible");
+//   }
+// }
 
 // my javascript for the website
 const btnSecricbe = document.querySelector(".btn-secricbe");
@@ -129,59 +210,41 @@ window.onscroll = function () {
   }
 
   if (scrollY >= 1000) {
-    // btnUp.style.display = "block";
     btnUp.classList.remove("hidden");
   } else {
     btnUp.classList.add("hidden");
-
-    // btnUp.style.display = "none";
   }
 };
 
 btnUp.addEventListener("click", () => {
   window.scrollTo(0, 0);
-  // btnUp.classList.add("hidden");
-  // btnUp.style.display = "none";
 });
 
 // Scroll Reavel
 
-
 const sr = ScrollReveal({
-    distance: "60px",
-    duration: 1000,
-    delay: 400,
-    // rest: true
+  distance: "60px",
+  duration: 1000,
+  delay: 400,
+  // rest: true
 });
-sr.reveal('.img-container',{origin: "left"});
-sr.reveal('.hero-content',{delay: 1000});
-sr.reveal('.who-im-content',{origin: "right"});
-sr.reveal('.youtube',{origin: "left"});
-sr.reveal('.row-1',{delay: 300});
-sr.reveal('.row-2',{delay: 600});
-sr.reveal('.row-3',{delay: 900});
-sr.reveal('.row-4',{delay: 1200});
-sr.reveal('.row-5',{delay: 1600});
-sr.reveal('.card-1',{delay: 500});
-sr.reveal('.card-2',{delay: 1500});
-sr.reveal('.card-3',{delay: 2500});
-sr.reveal('.item-1',{delay: 300});
-sr.reveal('.item-2',{delay: 600});
-sr.reveal('.item-3',{delay: 900});
-sr.reveal('.item-4',{delay: 1200});
-sr.reveal('.item-5',{delay: 1500});
-sr.reveal('.item-6',{delay: 1800});
+sr.reveal(".img-container", { origin: "left" });
+sr.reveal(".hero-content", { delay: 1000 });
+sr.reveal(".who-im-content", { origin: "right" });
+sr.reveal(".youtube", { origin: "left" });
+sr.reveal(".row-1", { delay: 300 });
+sr.reveal(".row-2", { delay: 600 });
+sr.reveal(".row-3", { delay: 900 });
+sr.reveal(".row-4", { delay: 1200 });
+sr.reveal(".row-5", { delay: 1600 });
+sr.reveal(".card-1", { delay: 500 });
+sr.reveal(".card-2", { delay: 1500 });
+sr.reveal(".card-3", { delay: 2500 });
+sr.reveal(".item-1", { delay: 300 });
+sr.reveal(".item-2", { delay: 600 });
+sr.reveal(".item-3", { delay: 900 });
+sr.reveal(".item-4", { delay: 1200 });
+sr.reveal(".item-5", { delay: 1500 });
+sr.reveal(".item-6", { delay: 1800 });
 
-ScrollReveal({distance: "0px"}).reveal('.section_header');
-
-// sr.reveal(`.home_header, .section_title`, { delay: 600 });
-// sr.reveal(`.home_footer`, { delay: 700 });
-// sr.reveal(`.home_img`, { delay: 900, origin: "top" });
-
-// sr.reveal(
-//     `.sponsor_img, .products_card, .footer_logo, .footer_content, .footer_copy`, { origin: "top", interval: 100 }
-// );
-// sr.reveal(`.specs_data, .discount_animate`, { origin: "left", interval: 100 });
-// sr.reveal(`.specs_img, .discount_img`, { origin: "right" });
-// sr.reveal(`.case_img`, { origin: "top" });
-// sr.reveal(`.case_data`);
+ScrollReveal({ distance: "0px" }).reveal(".section_header");
